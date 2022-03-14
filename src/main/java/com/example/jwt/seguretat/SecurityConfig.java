a7d1441275cd8f8965ee1f05c42ab7cb6dbc0cd2
oriol.sanchez.7e4 <oriol.sanchez.7e4@itb.cat>
2022-03-14 20:07:17 +0100

    commit

--- src/main/java/com/example/jwt/seguretat/SecurityConfig.java
+++ src/main/java/com/example/jwt/seguretat/SecurityConfig.java
@@ -39,15 +39,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
 //    }
 
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception
-    {
-        http
-                .csrf().disable()
-                .authorizeRequests().anyRequest().authenticated()
-                .and()
-                .httpBasic();
-    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception
+//    {
+//        http
+//                .csrf().disable()
+//                .authorizeRequests().anyRequest().authenticated()
+//                .and()
+//                .httpBasic();
+//    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
